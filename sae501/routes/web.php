d646c61ab292aa90c7732971d85c1d63d5b70c8a
HOFFMANN CELIA <celia.hoffmann@etu.unistra.fr>
2025-09-24 15:25:56 +0200

    création de pages pour la connexion et inscription + mise en page de base

--- sae501/routes/web.php
+++ sae501/routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/connexion', function () {
+    return view('connexion');
+});
+
+Route::get('/inscription', function () {
+    return view('inscription');
 });
